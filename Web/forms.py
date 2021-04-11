6c3bd2164686860d9e04c39878a6334d21f06a5d
hoang ha <churuanho0o0@gmail.com>
2021-04-11 10:40:37 +0700

    version 2.0

--- Web/forms.py
+++ Web/forms.py
@@ -2,5 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class EnterForm(FlaskForm):
-    ID = StringField(label="ID of 'phong thi'")
-    submit = SubmitField(label='submit')
+    roomID = StringField(label="Room")
+    date = StringField(label="Date")
+    time = StringField(label="Time")
+    submit = SubmitField(label='Submit')
